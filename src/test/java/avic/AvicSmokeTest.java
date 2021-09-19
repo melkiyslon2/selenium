fef6f7c2bf08e91d3cda55ec25842eb6a6e39d4e
melkiyslon2 <40568979+melkiyslon2@users.noreply.github.com>
2021-09-19 16:24:08 +0300

    Update AvicSmokeTest.java

--- src/test/java/avic/AvicSmokeTest.java
+++ src/test/java/avic/AvicSmokeTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -16,7 +15,6 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class AvicSmokeTest {
@@ -43,23 +41,28 @@ public class AvicSmokeTest {
         driver.findElement(By.xpath("//input[contains(@class,'form-control-min')]")).sendKeys("24200", Keys.ENTER);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//div[contains(@class,'open-filter-tooltip')]//a[@class='filter-tooltip js_filters_accept']")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class='prod-cart__descr']"));
+        for (WebElement element: elements) {
+            assertTrue(element.getText().contains("Apple iPhone"));
+        }
     }
 
     @Test(priority = 2)
-    public void changeStorage() {
+    public void changeColorOfEarPhones() {
         driver.findElement(By.xpath("//input[@id='input_search']")).sendKeys("Pixel");
         driver.findElement(By.xpath("//button[@class='button-reset search-btn']")).click();
         driver.findElement(By.xpath("//a[@title='Наушники Google Pixel Buds 2 White']")).click();
-        //driver.findElement(By.xpath("")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//a[contains(@data-ecomm-cart,'Наушники Google Pixel Buds 2 Black')]")).click();
-        WebDriverWait wait = new WebDriverWait(driver,3000);
+        driver.findElement(By.xpath("//a[contains(@data-ecomm-cart,'Наушники Google Pixel Buds 2 White')]")).click();
+        WebDriverWait wait = new WebDriverWait(driver,30);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("js_cart")));
         driver.findElement(By.xpath("//a[contains(text(),'Продолжить покупки')]")).click();
+        String color = driver.findElement(By.xpath("//span[@class='name']")).getText();
+        assertTrue(color.contains("Google Pixel Buds"));
     }
 
     @Test(priority = 3)
-    public void testShopsInKharkov(){
+    public void searchShopsInKharkov(){
         driver.findElement(By.xpath("//span[contains(text(),'Электротранспорт')]")).click();
         driver.findElement(By.xpath("//div[contains(text(),'Где купить Электротранспорт?')]")).click();
         List<WebElement> elementsList = driver.findElements(By.xpath("//div[@class='faq__question accord-box open']//ul/li[contains(text(),'Харьков')]"));
